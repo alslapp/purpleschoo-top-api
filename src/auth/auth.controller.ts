6724bd3007fa0227873d3d994f550a296825b98e
al.slapp <al.slapp@gmail.com>
2023-08-03 14:42:55 +0300

    initial structure created

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import {
+	Body,
+	Controller,
+	HttpCode,
+	HttpStatus,
+	Post,
+} from '@nestjs/common';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+	@Post('register')
+	async register(@Body() dto: AuthDto) {
+
+	}
+
+	@HttpCode(HttpStatus.OK)
+	@Post('login')
+	async login() {
+
+	}
+}
